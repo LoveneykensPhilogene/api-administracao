cd33a49623de0d59283d90137e58ec5a2db0c31d
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-06 02:08:20 -0300

    Tabela Setor criada no banco de dados

--- backend/src/main/java/br/backend/api_selecao/entities/Setor.java
+++ backend/src/main/java/br/backend/api_selecao/entities/Setor.java
@@ -2,11 +2,20 @@ package br.backend.api_selecao.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Setor implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 
 	public Setor() {
