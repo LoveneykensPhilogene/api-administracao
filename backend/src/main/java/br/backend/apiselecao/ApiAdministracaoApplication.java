207954425862bfb44b34b62228b6fa85cea41393
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-06 00:31:50 -0300

    projeto criado

---
+++ backend/src/main/java/br/backend/apiselecao/ApiAdministracaoApplication.java
@@ -0,0 +1,13 @@
+package br.backend.apiselecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdministracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdministracaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/backend/apiselecao/ApiAdministracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.backend.apiselecao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdministracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
