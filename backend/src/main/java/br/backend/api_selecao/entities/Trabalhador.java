31ec0ff7cafc83199f4d59112c6c04d2b0f1e0e0
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-06 01:02:08 -0300

    classe Trabalhador criada

---
+++ backend/src/main/java/br/backend/api_selecao/entities/Trabalhador.java
@@ -0,0 +1,84 @@
+package br.backend.api_selecao.entities;
+
+import java.io.Serializable;
+
+public class Trabalhador implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String nome;
+
+	private String cpf;
+
+	private String sexo;
+
+	public Trabalhador() {
+	}
+
+	public Trabalhador(Long id, String nome, String cpf, String sexo) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trabalhador other = (Trabalhador) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
