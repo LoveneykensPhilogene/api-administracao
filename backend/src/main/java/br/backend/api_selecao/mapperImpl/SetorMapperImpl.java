01c8ff94b81698f7b3b298dbe617f145a146cb86
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-10 21:06:48 -0300

    classes excluidas

--- backend/src/main/java/br/backend/api_selecao/mapperImpl/SetorMapperImpl.java
+++ backend/src/main/java/br/backend/api_selecao/mapperImpl/SetorMapperImpl.java
@@ -1,8 +1,13 @@
 package br.backend.api_selecao.mapperImpl;
+
 import org.springframework.stereotype.Service;
 
+import br.backend.api_selecao.dto.CargoDto;
 import br.backend.api_selecao.dto.SetorDto;
+import br.backend.api_selecao.dto.TrabalhadorDto;
+import br.backend.api_selecao.entities.Cargo;
 import br.backend.api_selecao.entities.Setor;
+import br.backend.api_selecao.entities.Trabalhador;
 import br.backend.api_selecao.mapper.SetorMapper;
 
 @Service
@@ -18,8 +23,33 @@ public class SetorMapperImpl implements SetorMapper {
 	@Override
 	public SetorDto modelDto(Setor setor) {
 		SetorDto sDto = new SetorDto();
-		sDto.setId(setor.getId());
-		sDto.setNome(setor.getNome());
+		if (setor.getCargo() == null && setor.getTrabalhador() == null) {
+			sDto.setId(setor.getId());
+			sDto.setNome(setor.getNome());
+		} else if (setor.getCargo() != null && setor.getTrabalhador() == null) {
+			sDto.setId(setor.getId());
+			sDto.setNome(setor.getNome());
+			for (Cargo cargo : setor.getCargo()) {
+				sDto.getCargoDto().add(new CargoDto(cargo));
+			}
+		} else {
+
+			sDto.setId(setor.getId());
+			sDto.setNome(setor.getNome());
+
+			for (Cargo cargo : setor.getCargo()) {
+				if (cargo.getCargoTrabalhador() == null) {
+
+				} else {
+					sDto.getCargoDto().add(new CargoDto(cargo));
+				}
+			}
+
+			for (Trabalhador trabalhador : setor.getTrabalhador()) {
+				sDto.getTrabalhadorDto().add(new TrabalhadorDto(trabalhador));
+			}
+		}
+
 		return sDto;
 
 	}
