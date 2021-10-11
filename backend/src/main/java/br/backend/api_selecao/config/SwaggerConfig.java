9496a31ebb4399cb7173c141f2246c664c6afd4b
Loveneykens Philogene <loveneykens@gmail.com>
2021-10-10 21:01:42 -0300

    classe Swagger atualizada

--- backend/src/main/java/br/backend/api_selecao/config/SwaggerConfig.java
+++ backend/src/main/java/br/backend/api_selecao/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package br.backend.api_selecao.config;
 
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,31 +17,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	private static final String BASE_PACKAGE = "br.backend.api_selecao.controllers";
+	private static final String API_TITLE = "API ADMINISTRACAO";
+	private static final String API_DESCRIPTION = "REST API para cadastro";
+	private static final String CONTACT_NAME = "Loveneykens Philogene";
+	private static final String CONTACT_GITHUB = "https://github.com/LoveneykensPhilogene";
+	private static final String CONTACT_EMAIL = "loveneykens@gmail.com";
+
 	@Bean
+
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(basePackage(BASE_PACKAGE))
+				.paths(PathSelectors.any()).build().useDefaultResponseMessages(false).apiInfo(buildApiInfo());
 	}
-	/*
-	 * public Docket api() { return new Docket(DocumentationType.SWAGGER_2)
-	 * .select() .apis(RequestHandlerSelectors.basePackage(
-	 * "br.backend.api_selecao.controllers")) .paths(PathSelectors.any()) .build()
-	 * .useDefaultResponseMessages(false)
-	 * 
-	 * .securitySchemes(Arrays.asList(new ApiKey("Token Access",
-	 * HttpHeaders.AUTHORIZATION, In.HEADER.name())))
-	 * 
-	 * .apiInfo(apiInfo()); //.globalResponseMessage(RequestMethod.GET,
-	 * responseMessageForGET())
-	 * .securityContexts(Arrays.asList(securityContext())) }
-	 * 
-	 * private ApiInfo apiInfo() { return new ApiInfoBuilder()
-	 * .title("Simple Spring Boot REST API") .description("") .version("1.0.0")
-	 * .license("Apache License Version 2.0")
-	 * .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-	 * 
-	 * .build(); 
-	 )) }
-	 */
+
+	private ApiInfo buildApiInfo() {
+		return new ApiInfoBuilder().title(API_TITLE).description(API_DESCRIPTION).version("1.0.0")
+				.contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL)).build();
+	}
 
 }
